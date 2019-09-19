d2fa964b2ad5b95af7403470012412c74298dbe2
VictorLatriglia <vlatrigliadiaz@gmail.com>
2019-09-18 21:51:43 -0500

    recognition each second and ready to be used for position calculation

--- object_detected.py
+++ object_detected.py
@@ -1,9 +1,9 @@
 class ObjectDetected:
-    startX=0
-    endX=0
-    startY=0
-    endY=0
-    label=""
+    startX = 0
+    endX = 0
+    startY = 0
+    endY = 0
+    label = ""
     def __init__(self, startX,endX,startY,endY,label):
         self.startX = startX
         self.endX = endX
